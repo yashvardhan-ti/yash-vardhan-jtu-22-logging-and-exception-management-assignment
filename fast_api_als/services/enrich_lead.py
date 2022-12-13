06c649c2724eb234a736945259147eb7c0c6f2a2
yashvardhan-ti <yash.vardhan@trilogy.com>
2022-12-13 11:47:02 +0000

    add exceptions to enrich_lead

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -1,6 +1,7 @@
 import calendar
 import time
 import logging
+import math
 from dateutil import parser
 from fast_api_als.database.db_helper import db_helper_session
 
@@ -12,4 +13,6 @@ what exceptions can be thrown here?
 
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    pass
+    if adf_json is not dict:
+        raise ValueError(f'given paramerter {adf_json} is invalid')
+        return
