49788267a3cc6c9d80c0823e1afc11f3340f286f
yashvardhan-ti <yash.vardhan@trilogy.com>
2022-12-15 06:22:10 +0000

    add keyEror for enrich_lead jsoon

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -15,4 +15,8 @@ what exceptions can be thrown here?
 def get_enriched_lead_json(adf_json: dict) -> dict:
     if adf_json is not dict:
         raise ValueError(f'given paramerter {adf_json} is invalid')
-        return
+    try:
+        key = input('search for key: ')
+        print(f'The value for {key} key is {adf_json[key]}')
+    except KeyError:
+        print(f'The value for {key} does not exist'.format(key))
