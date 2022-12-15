957c1d09e814a081fdf79c2e6bd0912075bd8e58
yashvardhan-ti <yash.vardhan@trilogy.com>
2022-12-15 07:10:41 +0000

    add logging and exceptions in adf.py

--- fast_api_als/utils/adf.py
+++ fast_api_als/utils/adf.py
@@ -4,7 +4,7 @@ import logging
 from uszipcode import SearchEngine
 import re
 
-
+logging.basicConfig(filename='adf.log', format="%(levelname)s: %(message)s", level=logging.DEBUG)
 
 # ISO8601 datetime regex
 regex = r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$'
@@ -39,8 +39,12 @@ def is_nan(x):
 
 def parse_xml(adf_xml):
     # use exception handling
-    obj = xmltodict.parse(adf_xml)
-    return obj
+    try:
+        obj = xmltodict.parse(adf_xml)
+        return obj
+    except Exception as e:
+        logging.info(f'The adf_xml could not be parsed')
+        raise e
 
 
 def validate_adf_values(input_json):
@@ -67,6 +71,7 @@ def validate_adf_values(input_json):
     # zipcode validation
     res = zipcode_search.by_zipcode(zipcode)
     if not res:
+        logging.error(f'The zipcode {zipcode} is invalid')
         return {"status": "REJECTED", "code": "4_INVALID_ZIP", "message": "Invalid Postal Code"}
 
     # check for TCPA Consent
@@ -75,10 +80,12 @@ def validate_adf_values(input_json):
         if id['@source'] == 'TCPA_Consent' and id['#text'].lower() == 'yes':
             tcpa_consent = True
     if not email and not tcpa_consent:
+        logging.error(f'The contact method with email {email} is missing TCPA consent')
         return {"status": "REJECTED", "code": "7_NO_CONSENT", "message": "Contact Method missing TCPA consent"}
 
     # request date in ISO8601 format
     if not validate_iso8601(input_json['requestdate']):
+        logging.error(f'The DateTime format is invalid')
         return {"status": "REJECTED", "code": "3_INVALID_FIELD", "message": "Invalid DateTime"}
 
     return {"status": "OK"}
